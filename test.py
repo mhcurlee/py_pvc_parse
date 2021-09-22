4707bb92ea6e329232b46a5677c31fbfe9ed8601
Marvin Curlee <marvin.curle@gmail.com>
2021-09-21 22:21:11 -0500

    simple refactor

--- test.py
+++ test.py
@@ -11,36 +11,40 @@ basepath = "/pub/base/"
 data = []
 
 
-# Read template file
-with open('pv.yaml') as file_:
-    template = Template(file_.read())
-
-
-# Read pvc file
-with open('pvc.yaml') as file_:
-    pvc_filedata = file_.read()
-    pvc_data = safe_load(pvc_filedata)
-
-
 # render template
 def gen_yaml(name,size,mode,server,path): 
   ''' Function to render yaml from template'''
   print(template.render(name=name,size=size, mode=mode, server=server, path=path))
 
 
-for item in pvc_data['items']:
-  item_dict = {
-    "name": namespace_name + "-" + item['metadata']['name'] + "-pv", 
-    "size": item['spec']['resources']['requests']['storage'],
-    "mode": item['spec']['accessModes'][0],
-    "server": server,
-    "path": basepath + namespace_name + "/" + item['metadata']['name']
-    }
-  data.append(item_dict)
+# Start
+if __name__ == "__main__":
+
+  # Read template file
+  with open('pv.yaml') as file_:
+      template = Template(file_.read())
 
 
-# loop through array and render template
-for item in data:
-  gen_yaml(item["name"], item["size"], item["mode"], item["server"], item["path"])
+  # Read pvc file
+  with open('pvc.yaml') as file_:
+      pvc_filedata = file_.read()
+      pvc_data = safe_load(pvc_filedata)
+      
+
+  # loop through list of dicts
+  for item in pvc_data['items']:
+    item_dict = {
+      "name": namespace_name + "-" + item['metadata']['name'] + "-pv", 
+      "size": item['spec']['resources']['requests']['storage'],
+      "mode": item['spec']['accessModes'][0],
+      "server": server,
+      "path": basepath + namespace_name + "/" + item['metadata']['name']
+      }
+    data.append(item_dict)
+
+
+  # loop through array and render template
+  for item in data:
+    gen_yaml(item["name"], item["size"], item["mode"], item["server"], item["path"])
 
 
